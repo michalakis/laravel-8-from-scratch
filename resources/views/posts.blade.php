6b1133ed746ec2414a66bd23577c50513b2e141e
Michalis Efstathiou <michalis@ROBIN.localdomain>
2021-05-20 09:46:33 +0300

    Episode 12

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/app.css">
+
+    <title>My Blog</title>
+</head>
+<body>
+    <?php foreach ($posts as $post) : ?>
+        <article>
+            <a href="/posts/<?= $post->slug ?>">
+                <h1><?= $post->title; ?></h1>
+            </a>
+
+            <div>
+                <?= $post->body; ?>
+            </div>
+
+        </article>
+    <?php endforeach; ?>
+</body>
+</html>
+
